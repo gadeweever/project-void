78835a024ff6373e8582afbc70a8aeec040207f6
gadeweever <gamaldeweever@gmail.com>
2014-07-07 23:52:21 -0700

    TOTALLY ADDED A FRISBEE
    YEAH. THIS IS AWESOME. SO COOL. WOW.

---
+++ project-void-source/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		if(KeyCode.Alpha0)
+		{
+			
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
