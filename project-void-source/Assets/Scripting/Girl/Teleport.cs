87e364efca48ae43ac1e7c585e38409998c36330
gadeweever <gamaldeweever@gmail.com>
2014-07-11 21:46:32 -0700

    Teleportish
    Teleportclass beginning. Will beef up as the game progresses and change
    mechanics as needed with a coroutine

---
+++ project-void-source/Assets/Scripting/Girl/Teleport.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+	
+	
+	public float TeleportSpeed;
+	public float Smooting;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DoTeleport();
+	}
+	
+	void DoTeleport()
+	{
+		if(Input.GetKey(KeyCode.Alpha1))
+		{
+			
+			Vector3 endlocation =  CheckObjectInPlace();
+			//if we find something, don't teleport
+			//do something more interesting to notify the player they can't
+			if(Vector3.Equals(endlocation,transform.position))
+			{
+				Debug.Log("You can't teleport there");
+				return;
+			}
+			
+			Debug.Log("the endlocation is " + endlocation.ToString());
+	
+			transform.position = Vector3.Lerp(transform.position, endlocation, TeleportSpeed * Smooting * Time.deltaTime);
+		}
+	}
+	
+	
+	//checks if the player targets an area that she cannot teleprt into, I.E., on an object
+	Vector3 CheckObjectInPlace()
+	{
+		Ray checker = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(checker, out hit))
+		{
+			Debug.Log("We hit something at " + hit.point.ToString());
+			
+			//for now, return false if we hit the backdrop.
+			//update when objects are resolved.
+			if(hit.collider.tag == "BackGround")
+				return hit.point;
+			
+		}
+		
+		return transform.position;
+	}
+}
